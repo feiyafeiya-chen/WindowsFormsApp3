e2feb5d78a31009f55c094b069d757b1f0eb4abe
feiyafeiya-chen <chenqinyu2671@126.com>
2019-12-28 09:51:18 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.zhangai_name = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.obj_dst_name = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.obj_distance = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.licheng_name = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.v_sudu = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(241, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地铁列车自主定位及主动防护系统 V1.0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseMnemonic = false;
+            // 
+            // zhangai_name
+            // 
+            this.zhangai_name.AutoSize = true;
+            this.zhangai_name.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.zhangai_name.Location = new System.Drawing.Point(347, 116);
+            this.zhangai_name.Name = "zhangai_name";
+            this.zhangai_name.Size = new System.Drawing.Size(115, 21);
+            this.zhangai_name.TabIndex = 1;
+            this.zhangai_name.Text = "前方无障碍";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(214, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "前方到站";
+            // 
+            // obj_dst_name
+            // 
+            this.obj_dst_name.AutoSize = true;
+            this.obj_dst_name.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.obj_dst_name.Location = new System.Drawing.Point(314, 208);
+            this.obj_dst_name.Name = "obj_dst_name";
+            this.obj_dst_name.Size = new System.Drawing.Size(94, 21);
+            this.obj_dst_name.TabIndex = 3;
+            this.obj_dst_name.Text = "理工大学";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(454, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "距离停车";
+            // 
+            // obj_distance
+            // 
+            this.obj_distance.AutoSize = true;
+            this.obj_distance.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.obj_distance.Location = new System.Drawing.Point(546, 208);
+            this.obj_distance.Name = "obj_distance";
+            this.obj_distance.Size = new System.Drawing.Size(21, 21);
+            this.obj_distance.TabIndex = 5;
+            this.obj_distance.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(574, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "m";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(214, 327);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 21);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "里程";
+            // 
+            // licheng_name
+            // 
+            this.licheng_name.AutoSize = true;
+            this.licheng_name.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.licheng_name.Location = new System.Drawing.Point(266, 327);
+            this.licheng_name.Name = "licheng_name";
+            this.licheng_name.Size = new System.Drawing.Size(87, 21);
+            this.licheng_name.TabIndex = 8;
+            this.licheng_name.Text = "K25+864";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(454, 327);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 21);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "速度";
+            // 
+            // v_sudu
+            // 
+            this.v_sudu.AutoSize = true;
+            this.v_sudu.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.v_sudu.Location = new System.Drawing.Point(533, 326);
+            this.v_sudu.Name = "v_sudu";
+            this.v_sudu.Size = new System.Drawing.Size(32, 21);
+            this.v_sudu.TabIndex = 10;
+            this.v_sudu.Text = "32";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(557, 326);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 21);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "km/h";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.v_sudu);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.licheng_name);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.obj_distance);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.obj_dst_name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.zhangai_name);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "K25+864";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label zhangai_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label obj_dst_name;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label obj_distance;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label licheng_name;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label v_sudu;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
